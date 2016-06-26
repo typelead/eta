b465e32868248603e091efff2eb4ea6072493728
Rahul Muttineni <rahulmutt@gmail.com>
2016-06-26 07:50:35 +0530

    Added script for debugging

---
+++ debug.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+if [ ! -f "./rts/build/rts.jar" ]; then
+    echo "Error: rts.jar not found!"
+    echo "Run ./build.sh first."
+fi
+
+if [ ! -f "./sample/build/mapandsum.jar" ]; then
+    echo "Error: mapandsum.jar not found!"
+    echo "Run ./build.sh first."
+fi
+
+jdb -classpath "rts/build/rts.jar:sample/build/mapandsum.jar" mapandsum.Main
