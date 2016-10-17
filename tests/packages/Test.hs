cbe3454879aebd21dc3fce51139b1f55f2f7809f
Sibi Prabakaran <sibi@psibi.in>
2016-10-18 01:26:05 +0530

    Test for ported packages

---
+++ tests/packages/Test.hs
@@ -0,0 +1,58 @@
+#!/usr/bin/env stack
+{- stack
+     --resolver lts-6.6
+     --install-ghc
+     runghc
+     --package turtle
+-}
+
+{-#LANGUAGE OverloadedStrings#-}
+
+module Test where
+
+import GHC.IO.Exception (ExitCode(..))
+import System.Exit (die)
+import Data.Monoid ((<>))
+import Data.Text (pack)
+import Turtle.Prelude (shell)
+
+-- TODO: Automatize this to fetch from ghcvm-hackage
+patchedPackages
+    :: [String]
+patchedPackages =
+    [ "array"
+    , "base-orphans"
+    , "binary"
+    , "bytestring"
+    , "containers"
+    , "filepath"
+    , "primitive"
+    , "random"
+    , "time"]
+
+-- TODO: Have a special file in ghcvm-package with this list to automatize it
+-- Packages that are working without any patching
+unPatchedPackages
+    :: [String]
+unPatchedPackages =
+    [ "Adaptive"
+    , "agum"
+    , "array-utils"
+    , "base-orphans"
+    , "base-prelude"
+    , "basic-lens"]
+
+buildPackage :: String -> IO ()
+buildPackage pkg = do
+    let buildCmd = "cabalvm install " <> (pack pkg)
+    exitCode <- shell buildCmd ""
+    case exitCode of
+        ExitSuccess -> return ()
+        ExitFailure x -> die ("error in building " <> pkg)
+    return ()
+
+main :: IO ()
+main = do
+    let packages = patchedPackages <> unPatchedPackages
+    shell "cabalvm update" ""
+    mapM_ buildPackage packages
