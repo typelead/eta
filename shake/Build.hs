34405376b2e37f3dabb8153e68e47f2905203334
Rahul Muttineni <rahulmutt@gmail.com>
2016-10-18 10:51:59 +0530

    Removed unnecessary comments in Build.hs

--- shake/Build.hs
+++ shake/Build.hs
@@ -198,9 +198,6 @@ main = shakeArgsWith shakeOptions{shakeFiles=rtsBuildDir} flags $ \flags targets
         unit $ cmd "ghcvm-pkg init " $ packageConfDir rootDir
         Stdout path <- cmd "stack eval GHC.Paths.libdir"
         let ghcLibPath = drop 1 (init (init path))
-            -- ghcLibPath = if os == "mingw32"
-            --              then ghcPath </> "lib"
-            --              else ghcPath </> "lib" </> "ghc-7.10.3"
             ghcInclude = ghcLibPath </> "include"
             ghcvmInclude = ghcvmIncludePath rootDir
         liftIO $ createDirectory ghcvmInclude
@@ -215,9 +212,6 @@ main = shakeArgsWith shakeOptions{shakeFiles=rtsBuildDir} flags $ \flags targets
         let sortedLibs = topologicalDepsSort libs getDependencies
         forM_ sortedLibs $ \lib ->
           buildLibrary debug lib (getDependencies lib)
-        -- putNormal $ "To finish the installation, add GHCVM_PACKAGE_PATH to your environment with the value '"
-        --          ++ packageConfDir rootDir
-        --          ++ "'."
 
     phony "test" $ do
       specs <- getDirectoryFiles "" ["//*.spec"]
