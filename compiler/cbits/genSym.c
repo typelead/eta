446f54cf41c3d504f77bc779320cde2ef74a3ee1
Rahul Muttineni <rahulmutt@gmail.com>
2016-10-25 13:26:07 +0530

    Added missing 'genSym' function

---
+++ compiler/cbits/genSym.c
@@ -0,0 +1,11 @@
+#include "Rts.h"
+
+static HsInt GenSymCounter = 0;
+
+HsInt genSym(void) {
+  if (n_capabilities == 1) {
+    return GenSymCounter++;
+  } else {
+    return atomic_inc((StgWord *)&GenSymCounter, 1);
+  }
+}
