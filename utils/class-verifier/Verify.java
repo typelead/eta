c1f3f0008c2d7153731a86dd13257081832531de
Rahul Muttineni <rahulmutt@gmail.com>
2016-12-03 11:09:10 +0530

    Updated verify script to crash upon exception

--- utils/class-verifier/Verify.java
+++ utils/class-verifier/Verify.java
@@ -3,14 +3,18 @@ import java.io.IOException;
 
 public class Verify {
     public static void main(String[] args) {
+        boolean error = false;
         try {
-            getAllFiles(args[0].replaceAll("/",".") + ".", new File(args[0]).getCanonicalFile());
+            error = getAllFiles(args[0].replaceAll("/",".") + ".", new File(args[0]).getCanonicalFile());
         } catch (IOException e) {
+            error = true;
             e.printStackTrace();
         }
+        if (error) System.exit(1);
     }
 
-    private static void getAllFiles(String prefix, File curDir) {
+    private static boolean getAllFiles(String prefix, File curDir) {
+        boolean error = false;
         File[] filesList = curDir.listFiles();
         for(File f : filesList){
             if(f.isFile()){
@@ -19,19 +23,24 @@ public class Verify {
                 try {
                     Class.forName(className);
                 } catch (ClassFormatError e) {
+                    error = true;
                     System.out.println(className);
                     e.printStackTrace();
                 } catch (ClassNotFoundException e) {
+                    error = true;
                     System.out.println(className);
                     e.printStackTrace();
                 } catch (VerifyError e) {
+                    error = true;
                     System.out.println(className);
                     e.printStackTrace();
                 } catch (NoClassDefFoundError e) {
+                    error = true;
                     System.out.println(className);
                     e.printStackTrace();
                 }
             }
         }
+        return error;
     }
 }
