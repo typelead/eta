46b09b952f1ca6adc8c77a4240e9b6d23a9da08b
Rahul Muttineni <rahulmutt@gmail.com>
2016-12-13 00:13:51 +0530

    Removed unnecessary dependencies

--- compiler/cbits/cutils.c
+++ compiler/cbits/cutils.c
@@ -1,4 +1,23 @@
 #include "Rts.h"
+#include <string.h>
+
+HsInt
+ghc_strlen( HsPtr a )
+{
+  return (strlen((char *)a));
+}
+
+HsInt
+ghc_memcmp( HsPtr a1, HsPtr a2, HsInt len )
+{
+  return (memcmp((char *)a1, a2, len));
+}
+
+HsInt
+ghc_memcmp_off( HsPtr a1, HsInt i, HsPtr a2, HsInt len )
+{
+  return (memcmp((char *)a1 + i, a2, len));
+}
 
 void
 enableTimingStats( void )       /* called from the driver */
