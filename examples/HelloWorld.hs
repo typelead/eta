e4e6d3a3ae5ec7fc81494d851987586ce16b71c9
Rahul Muttineni <rahulmutt@gmail.com>
2017-01-06 15:07:54 +0530

    Added examples directory

---
+++ examples/HelloWorld.hs
@@ -0,0 +1,5 @@
+-- Name: Hello World!
+-- Description: The standard hello world example for Eta.
+
+main :: IO () -- Note that this line is optional. Written for clarity.
+main = putStrLn "Hello World!"
