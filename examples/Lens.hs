6c54475ffb120b2bd84bbc882876b10b026f26f0
Rahul Muttineni <rahulmutt@gmail.com>
2017-01-06 22:46:26 +0530

    Added lens example

---
+++ examples/Lens.hs
@@ -0,0 +1,25 @@
+-- Name: Lens Example
+-- Description: Shows an example of how to use lens.
+import Control.Lens
+import Data.Char
+import qualified Data.Map as Map
+
+main :: IO ()
+main = do
+  print $ ("hello","world")^._2
+  print $ set _2 42 ("hello","world")
+  print $ ("hello",("world","!!!"))^._2._1
+  print $ set (_2._1) 42 ("hello",("world","!!!"))
+  print $ "hello"^.to length
+  print $ ("hello",("world","!!!"))^._2._2.to length
+  print $ _1 .~ "hello" $ ((),"world")
+  print $ ((), "world") & _1 .~ "hello"
+  print $ view _2 (10,20)
+  print $ Map.fromList [("hello","there")] ^.at "hello"
+  print $ Map.fromList [("hello","there")] & at "hello" ?~ "world"
+  print $ over mapped succ [1,2,3]
+  print $ over (mapped._2) succ [(1,2),(3,4)]
+  print $ _1.mapped._2.mapped %~ succ $ ([(42, "hello")],"world")
+  print $ both *~ 2 $ (1,2)
+  res <- mapMOf (traverse._2) (\xs -> length xs <$ putStrLn xs) [(42,"hello"),(56,"world")]
+  print res
