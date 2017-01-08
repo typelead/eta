78c62bac926a31687979227abb7518f53d58e36d
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-01-08 23:27:29 +0530

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Eta'
-copyright = u'2016-2017, Rahul Muttineni'
+copyright = u'2017, TypeLead'
 author = u'Rahul Muttineni'
 
 # The version info for the project you're documenting, acts as replacement for
