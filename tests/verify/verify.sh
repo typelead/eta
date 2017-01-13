f54df710e718c917203a180f901ac90de0511839
Rahul Muttineni <rahulmutt@gmail.com>
2017-01-13 23:52:57 +0530

    Update verify script for jars

--- tests/verify/verify.sh
+++ tests/verify/verify.sh
@@ -8,15 +8,14 @@ javac ../../utils/class-verifier/Verify.java
 cp ../../utils/class-verifier/Verify.class .
 echo "Verify.class built successfully."
 
-# Clean-up the previous result
-rm -rf build/ base/ integer/ ghczmprim/ cern/ eta/ main/
+# Remove old build artifacts
+rm -rf build
 
 # Compile a simple program and extract the files
 echo "Compiling a simple program..."
 echo "=== Eta Compiler Output ==="
 mkdir build
 eta -fforce-recomp -o build/Out.jar Main.hs
-# && jar xf build/Out.jar
 echo "===                     ==="
 echo "Compiled succesfully."
 
@@ -24,7 +23,6 @@ echo "Compiled succesfully."
 echo "Verifying the bytecode of compiled program..."
 echo "=== Verify Script Output ==="
 java Verify build/Out.jar
-# java Verify ghczmprim/ghc integer/ghc integer/ghc/integer integer/ghc/integer/biginteger integer/ghc/integer/logarithms base base/control base/control/concurrent base/control/exception base/control/monad base/control/monad/st base/control/monad/st/lazy base/data base/data/functor base/data/stref base/data/type base/data/typeable base/debug base/foreign base/foreign/foreignptr base/foreign/marshal base/ghc base/ghc/conc base/ghc/event base/ghc/fingerprint base/ghc/float base/ghc/io base/ghc/io/encoding base/ghc/io/handle base/ghc/rts base/numeric base/system base/system/console base/system/environment base/system/io base/system/mem base/system/posix base/text base/text/parsercombinators base/text/read base/text/show base/unsafe base/java
 echo "===                      ==="
 echo "Bytecode looking good."
 
--- utils/class-verifier/Verify.java
+++ utils/class-verifier/Verify.java
@@ -17,24 +17,25 @@ public class Verify {
     public static void main(String[] args) throws IOException,
                                                   MalformedURLException,
                                                   ClassNotFoundException {
-        String pathToJar = args[0];
-        JarFile jarFile = new JarFile(pathToJar);
-        Enumeration<JarEntry> e = jarFile.entries();
+        for (String pathToJar: args) {
+            JarFile jarFile = new JarFile(pathToJar);
+            Enumeration<JarEntry> e = jarFile.entries();
 
-        URL[] urls = { new URL("jar:file:" + pathToJar+"!/") };
-        URLClassLoader cl = URLClassLoader.newInstance(urls);
+            URL[] urls = { new URL("jar:file:" + pathToJar+"!/") };
+            URLClassLoader cl = URLClassLoader.newInstance(urls);
 
-        while (e.hasMoreElements()) {
-            JarEntry je = e.nextElement();
-            if(je.isDirectory() || !je.getName().endsWith(".class")){
-                continue;
+            while (e.hasMoreElements()) {
+                JarEntry je = e.nextElement();
+                if(je.isDirectory() || !je.getName().endsWith(".class")){
+                    continue;
+                }
+                // -6 because of .class
+                String className = je.getName().substring(0,je.getName().length()-6);
+                className = className.replace('/', '.');
+                Class c = cl.loadClass(className);
             }
-            // -6 because of .class
-            String className = je.getName().substring(0,je.getName().length()-6);
-            className = className.replace('/', '.');
-            Class c = cl.loadClass(className);
-        }
 
-        System.out.println("All classes passed verification.");
+            System.out.println("All classes passed verification.");
+        }
     }
 }
