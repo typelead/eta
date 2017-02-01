654fec983d801ed12785bc6ae7960a58d9f25975
Alexey Raga <alexey.raga@gmail.com>
2017-02-01 20:28:08 +1100

    do nothing if there are no changes

--- update.sh
+++ update.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
-git pull
-git submodule update --recursive
+_pull=$(git pull)
+_subs=$(git submodule update --recursive)
+
+if [ "$_pull" == "Already up-to-date." ] && [ -z "$_subs" ]; then
+	echo "Already up-to-date."
+	exit 0
+fi
+
 ./cleaninstall.sh
 epm update
