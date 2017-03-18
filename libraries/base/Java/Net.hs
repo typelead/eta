30d51d222aa4461525722d29dc5bcc1ecc7c9337
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-18 20:37:53 +0530

    Updated Java.Net
    Imported InetAddress NetworkInterface InterfaceAddress

--- libraries/base/Java/Net.hs
+++ libraries/base/Java/Net.hs
@@ -20,6 +20,7 @@ module Java.Net where
 import GHC.Base
 import GHC.Int
 import Java.Array
+import Java.Collections
 import Java.IO
 import Java.Primitive
 
@@ -71,3 +72,91 @@ foreign import java unsafe toExternalForm :: Java URL String
 -- TODO
 
 -- End java.net.URL
+
+-- Start java.net.InetAddress
+
+data {-# CLASS "java.net.InetAddress" #-} InetAddress = InetAddress (Object# InetAddress)
+  deriving Class
+
+foreign import java unsafe getAddress :: (b <: InetAddress) => Java b JByteArray
+
+foreign import java unsafe getCanonicalHostName :: (b <: InetAddress) => Java b String
+
+foreign import java unsafe getHostAddress :: (b <: InetAddress) => Java b String
+
+foreign import java unsafe getHostName :: (b <: InetAddress) => Java b String
+
+foreign import java unsafe isAnyLocalAddress :: (b <: InetAddress) => Java b Bool
+
+foreign import java unsafe isLinkLocalAddress :: (b <: InetAddress) => Java b Bool
+
+foreign import java unsafe isLoopbackAddress :: (b <: InetAddress) => Java b Bool
+
+foreign import java unsafe isMCGlobal :: (b <: InetAddress) => Java b Bool
+
+foreign import java unsafe isMCLinkLocal :: (b <: InetAddress) => Java b Bool
+
+foreign import java unsafe isMCNodeLocal :: (b <: InetAddress) => Java b Bool
+
+foreign import java unsafe isMCOrgLocal :: (b <: InetAddress) => Java b Bool
+
+foreign import java unsafe isMCSiteLocal :: (b <: InetAddress) => Java b Bool
+
+foreign import java unsafe isMulticastAddress :: (b <: InetAddress) => Java b Bool
+
+foreign import java unsafe isReachable :: (b <: InetAddress) => Int -> Java b Bool
+
+foreign import java unsafe "isReachable"
+  isReachableNI :: (b <: InetAddress) => NetworkInterface -> Int -> Int -> Java b Bool
+
+foreign import java unsafe isSiteLocalAddress :: (b <: InetAddress) => Java b Bool
+
+-- End java.net.InetAddress
+
+-- Start java.net.NetworkInterface
+
+data {-# CLASS "java.net.NetworkInterface" #-} NetworkInterface = NetworkInterface (Object# NetworkInterface)
+  deriving Class
+
+foreign import java unsafe getDisplayName :: Java NetworkInterface String
+
+foreign import java unsafe getHardwareAddress :: Java NetworkInterface JByteArray
+
+foreign import java unsafe getIndex :: Java NetworkInterface Int
+
+foreign import java unsafe getInetAddresses :: Java NetworkInterface (Enumeration InetAddress)
+
+foreign import java unsafe getInterfaceAddresses :: Java NetworkInterface (List InterfaceAddress)
+
+foreign import java unsafe getMTU :: Java NetworkInterface Int
+
+foreign import java unsafe getName :: Java NetworkInterface String
+
+foreign import java unsafe getParent :: Java NetworkInterface NetworkInterface
+
+foreign import java unsafe getSubInterfaces :: Java NetworkInterface (Enumeration NetworkInterface)
+
+foreign import java unsafe isLoopback :: Java NetworkInterface Bool
+
+foreign import java unsafe isPointToPoint :: Java NetworkInterface Bool
+
+foreign import java unsafe isUp :: Java NetworkInterface Bool
+
+foreign import java unsafe isVirtual :: Java NetworkInterface Bool
+
+foreign import java unsafe supportsMulticast :: Java NetworkInterface Bool
+
+-- End java.net.NetworkInterface
+
+-- Start java.net.InterfaceAddress
+
+data {-# CLASS "java.net.InterfaceAddress" #-} InterfaceAddress = InterfaceAddress (Object# InterfaceAddress)
+  deriving Class
+
+foreign import java unsafe "getAddress" getAddressIA :: Java InterfaceAddress Bool
+
+foreign import java unsafe getBroadcast :: Java InterfaceAddress Bool
+
+foreign import java unsafe getNetworkPrefixLength :: Java InterfaceAddress Short
+
+-- End java.net.InterfaceAddress
