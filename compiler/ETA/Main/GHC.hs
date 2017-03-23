36d7f7426e63f5464a6f43853fd37dc2d836116e
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2017-03-23 17:41:09 +0530

    Fixed Warnings in ETA.Main.GHC
    Removed trailing whitespace

--- compiler/ETA/Main/GHC.hs
+++ compiler/ETA/Main/GHC.hs
@@ -304,7 +304,7 @@ import ETA.Main.Annotations
 import ETA.BasicTypes.Module
 import ETA.Utils.UniqFM
 import ETA.Utils.Panic
-import ETA.Utils.Platform
+-- import ETA.Utils.Platform
 import ETA.Utils.Bag              ( unitBag )
 import ETA.Main.ErrUtils
 import ETA.Utils.MonadUtils
@@ -646,9 +646,9 @@ guessTarget str Nothing
         dflags <- getDynFlags
         liftIO $ throwGhcExceptionIO
                  (ProgramError (showSDoc dflags $
-                 text "target" <+> quotes (text file) <+> 
+                 text "target" <+> quotes (text file) <+>
                  text "is not a module name or a source file"))
-     where 
+     where
          (file,obj_allowed)
                 | '*':rest <- str = (rest, False)
                 | otherwise       = (str,  True)
@@ -661,7 +661,7 @@ guessTarget str Nothing
 
 -- | Inform GHC that the working directory has changed.  GHC will flush
 -- its cache of module locations, since it may no longer be valid.
--- 
+--
 -- Note: Before changing the working directory make sure all threads running
 -- in the same session have stopped.  If you change the working directory,
 -- you should also unload the current program (set targets to empty,
@@ -859,11 +859,11 @@ loadModule tcm = do
 
    mb_linkable <- case ms_obj_date ms of
                      Just t | t > ms_hs_date ms  -> do
-                         l <- liftIO $ findObjectLinkable (ms_mod ms) 
+                         l <- liftIO $ findObjectLinkable (ms_mod ms)
                                                   (ml_obj_file loc) t
                          return (Just l)
                      _otherwise -> return Nothing
-                                                
+
    let source_modified | isNothing mb_linkable = SourceModified
                        | otherwise             = SourceUnmodified
                        -- we can't determine stability here
@@ -1039,10 +1039,10 @@ getModuleInfo mdl = withSession $ \hsc_env -> do
 
 getPackageModuleInfo :: HscEnv -> Module -> IO (Maybe ModuleInfo)
 #ifdef GHCI
-getPackageModuleInfo hsc_env mdl 
+getPackageModuleInfo hsc_env mdl
   = do  eps <- hscEPS hsc_env
         iface <- hscGetModuleInterface hsc_env mdl
-        let 
+        let
             avails = mi_exports iface
             names  = availsToNameSet avails
             pte    = eps_PTE eps
@@ -1056,7 +1056,7 @@ getPackageModuleInfo hsc_env mdl
                         minf_instances = error "getModuleInfo: instances for package module unimplemented",
                         minf_iface     = Just iface,
                         minf_safe      = getSafeMode $ mi_trust iface,
-                        minf_modBreaks = emptyModBreaks  
+                        minf_modBreaks = emptyModBreaks
                 }))
 #else
 -- bogusly different for non-GHCI (ToDo)
@@ -1065,7 +1065,7 @@ getPackageModuleInfo _hsc_env _mdl = do
 #endif
 
 getHomeModuleInfo :: HscEnv -> Module -> IO (Maybe ModuleInfo)
-getHomeModuleInfo hsc_env mdl = 
+getHomeModuleInfo hsc_env mdl =
   case lookupUFM (hsc_HPT hsc_env) (moduleName mdl) of
     Nothing  -> return Nothing
     Just hmi -> do
@@ -1116,7 +1116,7 @@ modInfoLookupName minf name = withSession $ \hsc_env -> do
      Just tyThing -> return (Just tyThing)
      Nothing      -> do
        eps <- liftIO $ readIORef (hsc_EPS hsc_env)
-       return $! lookupType (hsc_dflags hsc_env) 
+       return $! lookupType (hsc_dflags hsc_env)
                             (hsc_HPT hsc_env) (eps_PTE eps) name
 
 modInfoIface :: ModuleInfo -> Maybe ModIface
@@ -1128,7 +1128,7 @@ modInfoSafe = minf_safe
 
 #ifdef GHCI
 modInfoModBreaks :: ModuleInfo -> ModBreaks
-modInfoModBreaks = minf_modBreaks  
+modInfoModBreaks = minf_modBreaks
 #endif
 
 isDictonaryId :: Id -> Bool
@@ -1295,11 +1295,11 @@ showRichTokenStream ts = go startLoc ts ""
 -- Interactive evaluation
 
 -- | Takes a 'ModuleName' and possibly a 'PackageKey', and consults the
--- filesystem and package database to find the corresponding 'Module', 
+-- filesystem and package database to find the corresponding 'Module',
 -- using the algorithm that is used for an @import@ declaration.
 findModule :: GhcMonad m => ModuleName -> Maybe FastString -> m Module
 findModule mod_name maybe_pkg = withSession $ \hsc_env -> do
-  let 
+  let
     dflags   = hsc_dflags hsc_env
     this_pkg = thisPackage dflags
   --
@@ -1322,7 +1322,7 @@ findModule mod_name maybe_pkg = withSession $ \hsc_env -> do
 
 modNotLoadedError :: DynFlags -> Module -> ModLocation -> IO a
 modNotLoadedError dflags m loc = throwGhcExceptionIO $ CmdLineError $ showSDoc dflags $
-   text "module is not loaded:" <+> 
+   text "module is not loaded:" <+>
    quotes (ppr (moduleName m)) <+>
    parens (text (expectJust "modNotLoadedError" (ml_hs_file loc)))
 
@@ -1366,7 +1366,7 @@ moduleTrustReqs m = withSession $ \hsc_env ->
     liftIO $ hscGetSafe hsc_env m noSrcSpan
 
 -- | EXPERIMENTAL: DO NOT USE.
--- 
+--
 -- Set the monad GHCi lifts user statements into.
 --
 -- Checks that a type (in string form) is an instance of the
@@ -1398,7 +1398,7 @@ obtainTermFromId bound force id = withSession $ \hsc_env ->
 -- entity known to GHC, including 'Name's defined using 'runStmt'.
 lookupName :: GhcMonad m => Name -> m (Maybe TyThing)
 lookupName name =
-     withSession $ \hsc_env -> 
+     withSession $ \hsc_env ->
        liftIO $ hscTcRcLookupName hsc_env name
 
 -- -----------------------------------------------------------------------------
@@ -1411,14 +1411,14 @@ parser :: String         -- ^ Haskell module source text (full Unicode is suppor
        -> FilePath       -- ^ the filename (for source locations)
        -> Either ErrorMessages (WarningMessages, Located (HsModule RdrName))
 
-parser str dflags filename = 
+parser str dflags filename =
    let
        loc  = mkRealSrcLoc (mkFastString filename) 1 1
        buf  = stringToStringBuffer str
    in
    case unP Parser.parseModule (mkPState dflags buf loc) of
 
-     PFailed span err   -> 
+     PFailed span err   ->
          Left (unitBag (mkPlainErrMsg dflags span err))
 
      POk pst rdr_module ->
