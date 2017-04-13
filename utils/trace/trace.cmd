4e73f91812e8da7bd45cbdd0801de06fb858f875
jneira <atreyu.bbb@gmail.com>
2017-04-13 23:01:04 +0200

    correct trace script

--- utils/trace/trace.cmd
+++ utils/trace/trace.cmd
@@ -1,2 +1,2 @@
 @echo off
-eta -o Out.jar %* && java -javaagent:slf4j-ext-1.7.21.jar=ignore=org/slf4j/:ch/qos/logback/:org/apache/log4j/:cern/colt/:java/-cp slf4j-api-1.7.21.jar;slf4j-simple-1.7.21.jar;javassist-3.20.0-GA.jar;Out.jar -Djava.compiler=NONE eta.main
+eta -o Out.jar %* && java -javaagent:slf4j-ext-1.7.21.jar=ignore=org/slf4j/:ch/qos/logback/:org/apache/log4j/:cern/colt/:java/ -cp slf4j-api-1.7.21.jar;slf4j-simple-1.7.21.jar;javassist-3.20.0-GA.jar;Out.jar -Djava.compiler=NONE eta.main
