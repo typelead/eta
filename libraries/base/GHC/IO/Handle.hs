ec247ffa6b19768dd0f4aea0bc97f76f0de758ae
Paavo <pparkkin@gmail.com>
2017-06-09 19:53:58 +0800

    Automatically select buffer mode in some cases.

--- libraries/base/GHC/IO/Handle.hs
+++ libraries/base/GHC/IO/Handle.hs
@@ -639,11 +639,7 @@ dupHandle_ :: (IODevice dev, BufferedIO dev, Typeable dev) => dev
 dupHandle_ new_dev filepath other_side h_@Handle__{..} mb_finalizer = do
    -- XXX wrong!
   mb_codec <- if isJust haEncoder then fmap Just getLocaleEncoding else return Nothing
-  is_tty <- IODevice.isTerminal new_dev
-  let buffer_mode
-         | is_tty = LineBuffering
-         | otherwise = BlockBuffering Nothing
-  mkHandle new_dev filepath haType buffer_mode mb_codec
+  mkHandle new_dev filepath haType Nothing mb_codec
       NewlineMode { inputNL = haInputNL, outputNL = haOutputNL }
       mb_finalizer other_side
 
