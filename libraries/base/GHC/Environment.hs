4470ede7725b4f5ba16d75cf9e2e484457ad8504
Rahul Muttineni <rahulmutt@gmail.com>
2017-08-15 22:16:02 -0400

    Implemented getFullArgs in GHC.Environment

--- libraries/base/GHC/Environment.hs
+++ libraries/base/GHC/Environment.hs
@@ -4,25 +4,15 @@
 
 module GHC.Environment (getFullArgs) where
 
-import Foreign
-import Foreign.C
 import GHC.Base
-import GHC.Real ( fromIntegral )
-
-import GHC.IO.Encoding
-import GHC.Num
-import qualified GHC.Foreign as GHC
+import GHC.Pack
+import Java
 
 getFullArgs :: IO [String]
-getFullArgs = undefined
-  -- TODO: Implement
-  -- alloca $ \ p_argc ->
-  -- alloca $ \ p_argv -> do
-  --  getFullProgArgv p_argc p_argv
-  --  p    <- fromIntegral `liftM` peek p_argc
-  --  argv <- peek p_argv
-  --  enc <- getFileSystemEncoding
-  --  peekArray (p - 1) (advancePtr argv 1) >>= mapM (GHC.peekCString enc)
+getFullArgs = do
+  jargs <- getFullArgs'
+  return (map unpackCString (fromJava jargs))
 
-getFullProgArgv :: Ptr CInt -> Ptr (Ptr CString) -> IO ()
-getFullProgArgv = undefined
+foreign import java unsafe "@static @field eta.runtime.RuntimeOptions.fullProgArgs"
+  getFullArgs' :: IO JStringArray
+
