37da9b6f883819ed97b808377f5012e5c1d3a199
Rahul Muttineni <rahulmutt@gmail.com>
2017-08-15 22:33:32 -0400

    Implemented getEnvironment in System.Environment

--- libraries/base/System/Environment.hs
+++ libraries/base/System/Environment.hs
@@ -50,6 +50,7 @@ import System.Posix.Internals (withFilePath)
 import GHC.Pack
 import System.Environment.ExecutablePath
 import Java
+import Java.Collections
 
 -- | Computation 'getArgs' returns a list of the program's command
 -- line arguments (not including the program name).
@@ -127,22 +128,8 @@ ioe_missingEnvVar name = ioException (IOError Nothing NoSuchThing "getEnv"
 --
 -- @since 4.7.0.0
 setEnv :: String -> String -> IO ()
-setEnv key_ value_
-  | null key       = throwIO (mkIOError InvalidArgument "setEnv" Nothing Nothing)
-  | '=' `elem` key = throwIO (mkIOError InvalidArgument "setEnv" Nothing Nothing)
-  | null value     = unsetEnv key
-  | otherwise      = setEnv_ key value
-  where
-    key   = takeWhile (/= '\NUL') key_
-    value = takeWhile (/= '\NUL') value_
-
-setEnv_ :: String -> String -> IO ()
-setEnv_ k v = putEnv (k ++ "=" ++ v)
-
--- TODO: If this functionally is REALLY required,
--- we can implement with our own env map.
-putEnv :: String -> IO ()
-putEnv keyvalue = undefined
+setEnv key value = error $ "setEnv " ++ key ++ "=" ++ value ++
+                           ": Cannot modify environment on JVM."
 
 -- | @unSet name@ removes the specified environment variable from the
 -- environment of the current process.
@@ -152,7 +139,7 @@ putEnv keyvalue = undefined
 --
 -- @since 4.7.0.0
 unsetEnv :: String -> IO ()
-unsetEnv key = setEnv_ key ""
+unsetEnv key = setEnv key ""
 
 {-|
 'withArgs' @args act@ - while executing action @act@, have 'getArgs'
@@ -176,43 +163,17 @@ withProgName nm act = do
 -- the duration of an action.
 
 withArgv :: [String] -> IO a -> IO a
+withArgv = error "withArgv: Cannot modify arguments."
+  -- withProgArgv
 
--- #ifdef mingw32_HOST_OS
--- -- We have to reflect the updated arguments in the RTS-side variables as
--- -- well, because the RTS still consults them for error messages and the like.
--- -- If we don't do this then ghc-e005 fails.
--- withArgv new_args act = withWin32ProgArgv new_args $ withProgArgv new_args act
--- #else
-withArgv = withProgArgv
--- #endif
-
-withProgArgv :: [String] -> IO a -> IO a
-withProgArgv new_args act = do
-  pName <- System.Environment.getProgName
-  existing_args <- System.Environment.getArgs
-  bracket (setProgArgv new_args)
-          (\argv -> do _ <- setProgArgv (pName:existing_args)
-                       freeProgArgv argv)
-          (const act)
-
-freeProgArgv :: Ptr CString -> IO ()
-freeProgArgv argv = undefined
-  --do
-  -- size <- lengthArray0 nullPtr argv
-  -- sequence_ [ peek (argv `advancePtr` i) >>= free
-  --           | i <- [size - 1, size - 2 .. 0]]
-  -- free argv
-
-setProgArgv :: [String] -> IO (Ptr CString)
-setProgArgv argv = undefined
-  --do
-  -- enc <- getFileSystemEncoding
-  -- vs <- mapM (GHC.newCString enc) argv >>= newArray0 nullPtr
-  -- c_setProgArgv (genericLength argv) vs
-  -- return vs
-
-c_setProgArgv  :: CInt -> Ptr CString -> IO ()
-c_setProgArgv = undefined
+-- withProgArgv :: [String] -> IO a -> IO a
+-- withProgArgv new_args act = do
+--   pName <- System.Environment.getProgName
+--   existing_args <- System.Environment.getArgs
+--   bracket (setProgArgv new_args)
+--           (\argv -> do _ <- setProgArgv (pName:existing_args)
+--                        freeProgArgv argv)
+--           (const act)
 
 -- |'getEnvironment' retrieves the entire environment as a
 -- list of @(key,value)@ pairs.
@@ -220,54 +181,10 @@ c_setProgArgv = undefined
 -- If an environment entry does not contain an @\'=\'@ character,
 -- the @key@ is the whole entry and the @value@ is the empty string.
 getEnvironment :: IO [(String, String)]
+getEnvironment = do
+  env <- getenv
+  return $ map (\(k, v) -> (unpackCString k, unpackCString v))
+         $ fromJava env
 
--- #ifdef mingw32_HOST_OS
--- getEnvironment = bracket c_GetEnvironmentStrings c_FreeEnvironmentStrings $ \pBlock ->
---     if pBlock == nullPtr then return []
---      else go pBlock
---   where
---     go pBlock = do
---         -- The block is terminated by a null byte where there
---         -- should be an environment variable of the form X=Y
---         c <- peek pBlock
---         if c == 0 then return []
---          else do
---           -- Seek the next pair (or terminating null):
---           pBlock' <- seekNull pBlock False
---           -- We now know the length in bytes, but ignore it when
---           -- getting the actual String:
---           str <- peekCWString pBlock
---           fmap (divvy str :) $ go pBlock'
-
---     -- Returns pointer to the byte *after* the next null
---     seekNull pBlock done = do
---         let pBlock' = pBlock `plusPtr` sizeOf (undefined :: CWchar)
---         if done then return pBlock'
---          else do
---            c <- peek pBlock'
---            seekNull pBlock' (c == (0 :: Word8 ))
-
--- foreign import WINDOWS_CCONV unsafe "windows.h GetEnvironmentStringsW"
---   c_GetEnvironmentStrings :: IO (Ptr CWchar)
-
--- foreign import WINDOWS_CCONV unsafe "windows.h FreeEnvironmentStringsW"
---   c_FreeEnvironmentStrings :: Ptr CWchar -> IO Bool
--- #else
-getEnvironment = undefined
-  --do
-   -- pBlock <- getEnvBlock
-   -- if pBlock == nullPtr then return []
-   --  else do
-   --    enc <- getFileSystemEncoding
-   --    stuff <- peekArray0 nullPtr pBlock >>= mapM (GHC.peekCString enc)
-   --    return (map divvy stuff)
-
-getEnvBlock :: IO (Ptr CString)
-getEnvBlock = undefined
--- #endif
-
-divvy :: String -> (String, String)
-divvy str =
-  case break (=='=') str of
-    (xs,[])        -> (xs,[]) -- don't barf (like Posix.getEnvironment)
-    (name,_:value) -> (name,value)
+foreign import java unsafe "@static java.lang.System.getenv" getenv
+  :: IO (Map JString JString)
