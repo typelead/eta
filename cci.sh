8a3290bd2f0df13b2549fbabae1084dd0a35b192
Rahul Muttineni <rahulmutt@gmail.com>
2018-07-08 13:18:26 +0530

    Avoid memory blowout with -j1

---
+++ cci.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+set -e
+
+INSTALL_DIR="$1"
+if [ \( -z "$INSTALL_DIR" \) -o \( "-" = "$INSTALL_DIR" \) ]
+then
+    INSTALL_DIR=`stack path --local-bin > /dev/null 2>&1 || echo "" > /dev/null`
+    if [ -z "$INSTALL_DIR" ]
+    then
+        INSTALL_DIR=`stack path --local-bin-path`
+    fi
+fi
+
+if [ "$#" -gt 0 ]; then shift; fi
+
+stack install eta:exe:eta eta-pkg etlas eta-build --local-bin-path="$INSTALL_DIR" -j1
+stack exec eta-build -- clean
+stack exec eta-build -- uninstall
+stack exec eta-build -- install "$@"
