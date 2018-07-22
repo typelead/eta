6ac2f6d56cdea09643c8c78920064d6e09d0e4f5
Rahul Muttineni <rahulmutt@gmail.com>
2018-07-22 19:51:42 +0530

    Have CachedBlock cache two blocks to make it more useful

--- rts/src/main/java/eta/runtime/storage/CachedBlock.java
+++ rts/src/main/java/eta/runtime/storage/CachedBlock.java
@@ -2,44 +2,69 @@ package eta.runtime.storage;
 
 public class CachedBlock {
     /* The cached block */
-    private Block block;
+    private Block block1;
     /* Start of previous received block. */
-    private long lowerAddress;
+    private long lowerAddress1;
     /* Start of the adjacent block. */
-    private long higherAddress;
+    private long higherAddress1;
+
+    /* The cached block */
+    private Block block2;
+
+    /* Start of previous received block. */
+    private long lowerAddress2;
+    /* Start of the adjacent block. */
+    private long higherAddress2;
+
+    /* We have two cached buffers because it was observed that in practice, there are
+       cases where you read from one address, do some transformation, and write to another
+       address in a different block. In such cases, the cache would get invalidated constantly
+       making it unnecessary overhead. */
+
+    /* Controls which cached block to overwrite next. */
+    private boolean nextInvalidation;
 
     private CachedBlock() {}
 
     /* Returns null if address is not within range. */
     private Block get(long address) {
-        if (address >= lowerAddress && address < higherAddress) {
-            return block;
+        /* TODO: Make this more efficient using the knowledge that the difference is constant
+                 (blockSize) and that blockSize will be a power of 2. */
+        if (address >= lowerAddress1 && address < higherAddress1) {
+            return block1;
+        } else if (address >= lowerAddress2 && address < higherAddress2) {
+            return block2;
+        } else {
+            return null;
         }
-        return null;
     }
 
     void set(Block block, long lowerAddress, long higherAddress) {
-        this.block = block;
-        this.lowerAddress = lowerAddress;
-        this.higherAddress = higherAddress;
+        if (nextInvalidation) {
+            this.block2 = block;
+            this.lowerAddress2 = lowerAddress;
+            this.higherAddress2 = higherAddress;
+        } else {
+            this.block1 = block;
+            this.lowerAddress1 = lowerAddress;
+            this.higherAddress1 = higherAddress;
+        }
+        nextInvalidation = !nextInvalidation;
     }
 
     /* Returns null if cache miss. */
     public static Block getBlock(long address) {
-        CachedBlock cb = cachedBlock.get();
-        if (cb == null) return null;
-        return cb.get(address);
+        return cachedBlock.get().get(address);
     }
 
     public static void setBlock(Block block) {
-        CachedBlock cb = cachedBlock.get();
-        if (cb == null) {
-            cb = new CachedBlock();
-            cachedBlock.set(cb);
-        }
-        block.fillCache(cb);
+        block.fillCache(cachedBlock.get());
     }
 
     private static final ThreadLocal<CachedBlock> cachedBlock
-        = new ThreadLocal<CachedBlock>();
+        = new ThreadLocal<CachedBlock>() {
+                public CachedBlock initialValue() {
+                    return new CachedBlock();
+                }
+            };
 }
