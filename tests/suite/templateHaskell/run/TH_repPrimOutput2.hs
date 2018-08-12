ffd19e138f70eb10013dd96dc649bda95d46197c
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2018-08-12 13:02:47 +0530

    Added tests for TH
    TH_repGuardOutput, TH_repPrimOutput, TH_repPrimOutput2

---
+++ tests/suite/templateHaskell/run/TH_repPrimOutput2.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE MagicHash, UnboxedTuples, TemplateHaskell #-}
+-- test the representation of unboxed literals
+
+module Main
+where
+
+import GHC.Exts
+import GHC.Float
+import Language.Haskell.TH
+import Text.PrettyPrint
+import System.IO
+
+main :: IO ()
+main = do putStrLn $ show $ $( do e <- [| 20# |]
+                                  [| I# $(return e) |] )
+          putStrLn $ show $ $( do e <- [| 32## |]
+                                  [| W# $(return e) |] )
+          putStrLn $ show $ $( do e <- [| 12.3# |]
+                                  [| F# $(return e) |] )
+          putStrLn $ show $ $( do e <- [| 24.6## |]
+                                  [| D# $(return e) |] )
