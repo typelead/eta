2be9bb5306877dce8442b28ead061af74e14084a
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2018-08-12 12:11:57 +0530

    [Ignored test] TH_overloadedlabel

--- tests/suite/Driver.hs
+++ tests/suite/Driver.hs
@@ -293,7 +293,7 @@ goldenVsFileDiff' name cmdf ref new act =
 ignored :: Set String
 ignored = S.fromList ["Echo", "FullExportTest", "tc141", "tc168", "tc211",
         "T14163", "T4912", "T6018", "T7541", "InstanceMethod", "TcTypeNatSimple",
-        "TcTypeSymbolSimple", "tcfail150", "tcfail205", "annrun01"]
+        "TcTypeSymbolSimple", "tcfail150", "tcfail205", "annrun01", "TH_overloadedlabels"]
 
 filterIgnored :: [FilePath] -> [FilePath]
 filterIgnored = filter f
