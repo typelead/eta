b9d947869d602b2368c3cd5b0fe01f8e82b2cac9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-07-31 10:15:58 -0400

    spelling: delimiters
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- compiler/Eta/Prelude/PrelInfo.hs
+++ compiler/Eta/Prelude/PrelInfo.hs
@@ -173,7 +173,7 @@ wiredInThings
 -- GHCi's ':info' command.
 lookupKnownNameInfo :: Name -> SDoc
 lookupKnownNameInfo name = case lookupNameEnv knownNamesInfo name of
-    -- If we do find a doc, we add comment delimeters to make the output
+    -- If we do find a doc, we add comment delimiters to make the output
     -- of ':info' valid Haskell.
     Nothing  -> empty
     Just doc -> vcat [text "{-", doc, text "-}"]
