fa798433aac05b76e340645fc0cdce16e2e041bf
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-07-31 10:17:38 -0400

    spelling: invisible
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- compiler/Eta/TypeCheck/TcHsType.hs
+++ compiler/Eta/TypeCheck/TcHsType.hs
@@ -895,7 +895,7 @@ Help functions for type applications
 
 addTypeCtxt :: LHsType Name -> TcM a -> TcM a
         -- Wrap a context around only if we want to show that contexts.
-        -- Omit invisble ones and ones user's won't grok
+        -- Omit invisible ones and ones user's won't grok
 addTypeCtxt (L _ ty) thing
   = addErrCtxt doc thing
   where
