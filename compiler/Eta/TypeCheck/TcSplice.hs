0f2308f99fd4987af0382fbf528a2b592a44922a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-07-31 10:15:22 -0400

    spelling: catching
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- compiler/Eta/TypeCheck/TcSplice.hs
+++ compiler/Eta/TypeCheck/TcSplice.hs
@@ -736,7 +736,7 @@ runMeta' show_code ppr_hs run_and_convert expr
                 -- including, say, a pattern-match exception in the code we are running
                 --
                 -- We also do the TH -> HS syntax conversion inside the same
-                -- exception-cacthing thing so that if there are any lurking
+                -- exception-catching thing so that if there are any lurking
                 -- exceptions in the data structure returned by hval, we'll
                 -- encounter them inside the try
                 --
