95500b95057d6db3fecc0f177eb24cba75bf99a6
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-07-31 10:18:37 -0400

    spelling: precisely
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- compiler/Eta/DeSugar/DeSugar.hs
+++ compiler/Eta/DeSugar/DeSugar.hs
@@ -448,7 +448,7 @@ That keeps the desugaring of list comprehensions simple too.
 
 
 Nor do we want to warn of conversion identities on the LHS;
-the rule is precisly to optimise them:
+the rule is precisely to optimise them:
   {-# RULES "fromRational/id" fromRational = id :: Rational -> Rational #-}
 
 
