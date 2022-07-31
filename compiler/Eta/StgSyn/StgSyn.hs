dc2e95de643882109537a86d3b7b3f2a4020fccf
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-07-31 10:17:55 -0400

    spelling: module
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- compiler/Eta/StgSyn/StgSyn.hs
+++ compiler/Eta/StgSyn/StgSyn.hs
@@ -485,7 +485,7 @@ Very like in @CoreSyntax@ (except no type-world stuff).
 The type constructor is guaranteed not to be abstract; that is, we can
 see its representation. This is important because the code generator
 uses it to determine return conventions etc. But it's not trivial
-where there's a moduule loop involved, because some versions of a type
+where there's a module loop involved, because some versions of a type
 constructor might not have all the constructors visible. So
 mkStgAlgAlts (in CoreToStg) ensures that it gets the TyCon from the
 constructors or literals (which are guaranteed to have the Real McCoy)
